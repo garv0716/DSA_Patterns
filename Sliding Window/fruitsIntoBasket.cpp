000323fbac83726ea84dc752cb6ddfd068c37bc4
Garv Gupta <garvg8423@gmail.com>
2026-05-26 23:45:46 +0530

    Add implementation of fruits into basket

---
+++ Sliding Window/fruitsIntoBasket.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+    int totalFruit(vector<int>& a) {
+       unordered_map<int,int> f;
+       int low=0,high=0;
+       int n=a.size();
+       int res=INT_MIN;
+       for(high=0;high<n;high++)
+       {
+           f[a[high]]++;
+           while(f.size()>2)
+           {
+               f[a[low]]--;
+               if(f[a[low]] ==0)
+               f.erase(a[low]);
+               low++;
+           }
+               int len=high-low+1;
+               res=max(res,len);
+          
+       }
+       return res;
+   }
+
+int main(){
+    vector<int>a = {1,2,3,2,2};
+    totalFruit(a);
+    return 0;
+}
