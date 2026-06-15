8005e45d8eb53a440cae82190d18c6c3ff75ff9e
Garv Gupta <garvg8423@gmail.com>
2026-06-15 23:56:23 +0530

    Add implementation of cycle detection in a linked list

---
+++ LInkedList/detectCycle.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+// Definition for singly-linked list
+struct ListNode {
+    int val;
+    ListNode* next;
+
+    ListNode(int x) {
+        val = x;
+        next = NULL;
+    }
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
+
+int main() {
+    // Creating nodes
+    ListNode* head = new ListNode(3);
+    ListNode* second = new ListNode(2);
+    ListNode* third = new ListNode(0);
+    ListNode* fourth = new ListNode(-4);
+
+    // Connecting nodes
+    head->next = second;
+    second->next = third;
+    third->next = fourth;
+
+    // Creating a cycle: last node points to second node
+    fourth->next = second;
+
+    Solution obj;
+
+    if (obj.hasCycle(head)) {
+        cout << "Cycle detected" << endl;
+    } else {
+        cout << "No cycle detected" << endl;
+    }
+
+    return 0;
+}
